e7d3b76b666f2139ba566d1d0c2d446e64c1c39e
BojanVasileski83 <bojanvasil@gmail.com>
2022-11-22 20:51:11 +0100

    work on chapter2

---
+++ Javascript/02_JavaScriptEssentials/EX2.2/main.js
@@ -0,0 +1,5 @@
+let myname = 'Bojan';
+let age = 38;
+let cancode = true;
+
+console.log('Hello my name is ' + myname + ', I am ' + age + ' years old and I can code JavaScript:' + cancode + '.' )
---
+++ exersize/exersize2/main.js
@@ -0,0 +1,9 @@
+
+let a = 10;
+let b = 15;
+
+let c = a/b;
+
+console.log(c);
+
+console.log("The sum of " + a + "and" + b + "is:" + c);
