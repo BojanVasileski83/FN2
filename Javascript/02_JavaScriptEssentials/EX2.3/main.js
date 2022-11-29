b7cc7a8a7ed2541096c45fe69e1c8b558a783786
BojanVasileski83 <bojanvasil@gmail.com>
2022-11-29 19:02:11 +0100

    work on chapter3

---
+++ Javascript/02_JavaScriptEssentials/EX2.3/main.js
@@ -0,0 +1,15 @@
+alert("Welcome");
+
+let a = prompt("Please enter the value for a");
+console.log(a);
+
+let b = prompt("Please enter the value for b");
+console.log(b);
+
+a = Number(a) ** 2;
+b = Math.pow(b,2);
+
+c = a + b;
+c = Math.sqrt(c);
+console.log(c);
+
---
+++ Javascript/02_JavaScriptEssentials/ЕX2.4/main.js
@@ -0,0 +1,22 @@
+alert("Exersize 2.4")
+
+let a=100;
+console.log(a);
+let b=20;
+console.log(b);
+let c=20;
+console.log(c);
+
+let action = prompt("Do you want to see the action?");
+
+//console.log(a +=b);
+a = a + b;
+console.log(a);
+
+//console.log(a /=c);
+a = a / c;
+console.log(a);
+
+//console.log(c %=b);
+c = c % b;
+console.log(c);
