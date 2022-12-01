aa3d1ce1c1d95e3885e2a0376371de789d8b6d72
BojanVasileski83 <bojanvasil@gmail.com>
2022-12-01 20:30:28 +0100

    work3.5

---
+++ Javascript/03_JavaScriptMultipleValues/EX3.5/main.js
@@ -0,0 +1,20 @@
+let people = {
+    friends: []
+};
+let miki = {
+    firstName: "Miki",
+    lastName: "Stojkovski",
+    ID: 1
+}
+let angela = {
+    firstName: "Angela",
+    lastName: "Gerovska",
+    ID: 2
+}
+let gjoko = {
+    firstName: "Gjoko",
+    lastName: "Mircevski",
+    ID: 3
+}
+people.friends.push(miki, angela, gjoko);
+console.log(people);
