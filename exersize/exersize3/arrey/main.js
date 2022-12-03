dd1267bad7e2bdd532a3b236566a89d301a45e42
BojanVasileski83 <bojanvasil@gmail.com>
2022-12-03 19:25:10 +0100

    arrayEX

---
+++ exersize/exersize3/arrey/main.js
@@ -0,0 +1,45 @@
+//let input = [1, 2, 3, 5];
+//arra1 = input.slice(0);
+//console.log(arra1);
+//3
+let input = [1, 2, 3, 5];
+arra1 = input.slice(2);
+console.log(arra1);
+//4
+let input = [1, 2, 3, 5];
+console.log(input[0]);
+//5
+let input = [1, 2, 3, 5, 9, 0];
+console.log(input[input.length - 1]);
+//6
+myColor = ["Red", "Green", "White", "Black"];
+console.log(myColor.toString());
+console.log(myColor.join());
+console.log(myColor.join("+"));
+//7
+let arr1 = [3, 8, 7, 6, 5, -4, 3, 2, 1];
+console.log(arr2);
+//8
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.reverse());
+//9
+let numbers = [6, 7, 9, 8, 10];
+let randomnumbers = numbers[Math.floor(Math.random() * numbers.length)];
+console.log(randomnumbers);
+//10
+let numbers = [6, 7, 9, 8, 10];
+let ninnumbers = Math.min.apply(Math, numbers);
+console.log(ninnumbers);
+//11
+let numbers = [6, 7, 9, 8, 10];
+let maxnumbers = Math.max.apply(Math, numbers);
+console.log(maxnumbers);
+//12
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let fruit1 = "Kiwi";
+fruits.push(fruit1);
+console.log(fruits);
+//13
+let Array1 = ["Ljupco", "Petko", "John", "Miles"];
+console.log(Array1.indexOf("Miles"));
+
