096470d4ab1aed0350b6c22e1b69f07223a3f76e
BojanVasileski83 <bojanvasil@gmail.com>
2022-12-03 20:30:23 +0100

    objektiEX

---
+++ exersize/exersize3/objekti/main.js
@@ -0,0 +1,51 @@
+let student = {
+     name: "David Rayy",
+     sclass: "VI",
+     rollno: 12,
+     };
+     console.log(
+       "The name is : " +
+         student.name +
+         "The class is: " +
+         student.sclass +
+        " the roll is:  " +
+         student.rollno
+     );
+//2
+let student = {
+    name: "David Rayy",
+    sclass: "VI",
+    rollno: 12,
+    };
+    console.log(student);
+    delete student.rollno;
+    console.log(student);
+//3
+let student = {
+    name: "David Rayy",
+    sclass: "VI",
+    rollno: 12,
+    };
+    console.log(student.name.length);
+//4
+let library = [
+       {
+         author: "Bill Gates",
+         title: "The Road Ahead",
+         readingStatus: true,
+       },
+       {
+         author: "Steve Jobs",
+         title: "Walter Isaacson",
+         readingStatus: true,
+       },
+       {
+         author: "Suzanne Collins",
+         title: "Mockingjay: The Final Book of The Hunger Games",
+         readingStatus: false,
+       },
+     ];
+     console.log(library[0].readingStatus);
+
+//5
+     
