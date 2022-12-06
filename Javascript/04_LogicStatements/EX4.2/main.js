b402cfc13356c0b4ae14c9c0df0b4f625219fd07
BojanVasileski83 <bojanvasil@gmail.com>
2022-12-06 19:08:43 +0100

    EX4.1

---
+++ Javascript/04_LogicStatements/EX4.2/main.js
@@ -0,0 +1,10 @@
+let rain = true;
+if (rain) {
+    console.log("Take unbrela");
+} else {console.log("Ostavi go home");
+}
+
+if (!rain) {
+    console.log("Take unbrela");
+} else {console.log("Ostavi go home");
+}
