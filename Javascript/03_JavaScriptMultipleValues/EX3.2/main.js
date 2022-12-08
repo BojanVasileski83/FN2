067accf0e3d915416008827ba25a9cfcfd84148f
BojanVasileski83 <bojanvasil@gmail.com>
2022-12-08 14:20:45 +0100

    Ex3.2

--- Javascript/03_JavaScriptMultipleValues/EX3.2/main.js
+++ Javascript/03_JavaScriptMultipleValues/EX3.2/main.js
@@ -1,22 +1,29 @@
-//kreiranje na prazen arej
+// kreiranje na prazen na arej
 let shoppingList = [];
-//dodavanje na vrednosti vo arejot
+// dodavanje na vrednisto vo arrejot
 shoppingList.push("Milk", "Bread", "Apples");
+console.log(shoppingList);
+// naoganje na pozicijata na vrednosta "Bread"
+let indexOfBread = shoppingList.indexOf("Bread");
+// nacin za updejtiranje na odredane vrednos vo arej
+// arr.splice(index na pozijata kade sakeme da dodademe, 
+//            broj na elementi sto sakame da gi izbriseme, 
+//            vrednostite sto gi dodavame : "Bananas", "Eggs");
+shoppingList.splice(indexOfBread, 1, "Bananas", "Eggs");
 
-//naogjanje na pozicijata vo vrednosta "Bread"
-let indexBread = shoppingList.indexOf("Bread");
-//nacin za apdejtiranje na odredena vrednost vo arej
-//arr.splice(index na pozicijata kade sakame da dodademe
-//          broj na elementi sto sakame da gi izbriseme
-//              )
-shoppingList.splice(indexOfBread, 1,"Bannanas", "Egges");
-console.log(shoppingList.pop());
+console.log("Poslediot izbrishan element: ",shoppingList.pop());
+
 shoppingList.sort();
+console.log(shoppingList);
 console.log(shoppingList.indexOf("Milk"));
-let indexOfBanana = shoppingList.indexOf("Bananas");
-shoppingList.splice(indexOfBanana + 1, 0,"Carrots", "Lettuce");
-let newshoppinglist = ["Juice", "Pop"];
-let combinedShoppinglist = shoppingList.concat(newshoppinglist, newshoppinglist);
-console.log(lastIndexOf);
-console.log(combinedShoppinglist);
 
+let indexOfBanana = shoppingList.indexOf("Bananas");
+shoppingList.splice(indexOfBanana + 1, 0, "Carrots","Lettuce");
+console.log(shoppingList);
+
+let newshoppingList = ["Juice", "Pop"];
+let combinedShoppingList = shoppingList.concat(newshoppingList, newshoppingList);
+let lastIndexOf = combinedShoppingList.lastIndexOf("Pop");
+
+console.log(lastIndexOf);
+console.log(combinedShoppingList);
