fee4da4f5e7dc73cd10ceb5e36ae4d77b0aa1916
BojanVasileski83 <bojanvasil@gmail.com>
2022-12-24 13:25:26 +0100

    func6.1

---
+++ Javascript/06_Functions/EX6.1/main.js
@@ -0,0 +1,11 @@
+function nameOfTheFunction(param1, param2) {
+    return param1 + param2;
+}
+number1 = 5;
+number2 = 10;
+
+let result = nameOfTheFunction(number1, number2);
+let result2 = nameOfTheFunction(5, 14);
+
+console.log(result);
+console.log(result2);
