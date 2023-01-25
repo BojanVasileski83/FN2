e57f8931d7225485e7691b5b9e8a01abfcc0f4f0
BojanVasileski83 <bojanvasil@gmail.com>
2023-01-25 18:57:32 +0100

    conditional

---
+++ Javascript/Exercises/w3resource/ConditionalStatmentsAndLoops/Ex2/main.js
@@ -0,0 +1,28 @@
+/*
+2. Write a JavaScript conditional statement to find the sign of product of three numbers. Display an alert box with the specified sign.
+Sample numbers : 3, -7, 2
+Output : The sign is -
+*/
+
+let num1 = 3;
+let num2 = -7;
+let num3 = 2;
+
+let product = num1 + num2 + num3;
+
+if (product > 0) {
+    console.log("The sign is +");
+} else {
+    console.log("The sign is -");
+}
+
+var x,y,chr;
+for(x=1; x <=6; x++)
+{
+   for (y=1; y < x; y++)
+     {
+    chr=chr+("*");        
+      }
+ console.log(chr);
+ chr='';    
+}
